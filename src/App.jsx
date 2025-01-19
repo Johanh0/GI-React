b3ac1616296f298eb56213de87afbebee24fba97
Johan Herrera <johanherrera20000@gmail.com>
2025-01-19 13:40:38 -0500

    Challenge very easy done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <div>
+          <h2>Johan Herrera</h2>
+          <h4>
+            email: <span>johan@gmail.com</span>
+          </h4>
+          <h4>
+            dathe of birth: <span>08/30/2000</span>
+          </h4>
+          <h4>
+            role: <span>software engineer</span>
+          </h4>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
